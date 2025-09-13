a53ec8a62a1b371baedcc1d7d4d3498504a13ab0
杨亚军 <yangyajun@haoce.com>
2025-09-13 17:30:06 +0800

    优化

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-import gulpError from './utils/gulpError';
 App({
     onShow() {
-        if (gulpError !== 'gulpErrorPlaceHolder') {
-            wx.redirectTo({
-                url: `/pages/gulp-error/index?gulpError=${gulpError}`,
-            });
-        }
+
     },
 });
